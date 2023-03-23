b9727424a61286d38faba63060571701f0641d65
Saman Pordanesh <sina.pordanesh@yahoo.com>
2023-03-23 11:47:07 -0600

    Database setting
    Create movie table and its relationships to the starring as one to many relationship

---
+++ Online-Cinema/database/migrations/2023_03_23_172014_create_movie_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovieTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('movie')){
+            Schema::create('movie', function (Blueprint $table) {
+                $table->id()->unique()->primary();
+                $table->timestamps();
+                $table->string("title");
+                $table->integer("releaseYear");
+                $table->integer("startDate");
+                $table->integer("endDate");
+                $table->string("director");
+                $table->integer("writer");
+                $table->string("studio");
+                $table->integer("price");
+                $table->integer("rating");
+                $table->integer("length");
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie');
+    }
+}
